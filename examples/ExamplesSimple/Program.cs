5a17cf4bc50045da643ab50a448578b0c5537460
Maxwell Weru <mburumaxwell@gmail.com>
2020-07-10 18:47:57 +0300

    Added a simple example

---
+++ examples/ExamplesSimple/Program.cs
@@ -0,0 +1,44 @@
+﻿using productboard;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExamplesSimple
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var options = new ProductboardClientOptions
+            {
+                Token = "your-token-here"
+            };
+            var client = new ProductboardClient(options);
+            var note = new Note
+            {
+                Title = "Note title",
+                Content = "Here is some <b>exciting</b> content",
+                CustomerEmail = "customer@example.com",
+                DisplayUrl = "https://www.example.com/deskdesk/notes/123",
+                Source = new Source
+                {
+                    Origin = "deskdesk",
+                    RecordId = "123",
+                },
+                Tags = new List<string>
+                {
+                    "3.0",
+                    "important",
+                    "experimental",
+                },
+            };
+            var response = await client.CreateNoteAsync(note);
+            var created = response.Resource;
+            Console.Write("Note created!");
+            Console.Write($" Id = {created.Data.Id}");
+            Console.WriteLine($"Url: {created.Links.Html}");
+        }
+    }
+}
