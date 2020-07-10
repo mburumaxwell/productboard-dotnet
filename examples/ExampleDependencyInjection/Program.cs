2870f3de8b489570dfa1bb970aec3fb1a57eaf7b
Maxwell Weru <mburumaxwell@gmail.com>
2020-07-10 18:51:20 +0300

    Added sample for DependencyInjection

---
+++ examples/ExampleDependencyInjection/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using productboard;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExampleDependencyInjection
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var token = "your-token-here";
+
+            var services = new ServiceCollection();
+            services.AddProductboard(token);
+
+            var provider = services.BuildServiceProvider();
+
+            var client = provider.GetRequiredService<ProductboardClient>();
+            var note = new Note
+            {
+                Title = "Note title",
+                Content = "Here is some <b>exciting</b> content",
+                CustomerEmail = "customer@example.com",
+                DisplayUrl = "https://www.example.com/deskdesk/notes/123",
+                Source = new Source
+                {
+                    Origin = "deskdesk",
+                    RecordId = "123",
+                },
+                Tags = new List<string>
+                {
+                    "3.0",
+                    "important",
+                    "experimental",
+                },
+            };
+            var response = await client.CreateNoteAsync(note);
+            var created = response.Resource;
+            Console.Write("Note created!");
+            Console.Write($" Id = {created.Data.Id}");
+            Console.WriteLine($"Url: {created.Links.Html}");
+        }
+    }
+}
