8fd0bf3edfe2a044793573e5409356167a7df643
Maxwell Weru <mburumaxwell@gmail.com>
2020-07-11 09:42:41 +0300

    Added example for GDPR

---
+++ examples/ExampleGDPR/Program.cs
@@ -0,0 +1,23 @@
+﻿using productboard;
+using System;
+using System.Threading.Tasks;
+
+namespace ExampleGDPR
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var options = new ProductboardGdprClientOptions
+            {
+                Token = "your-token-here"
+            };
+            var client = new ProductboardGdprClient(options);
+
+            var response = await client.DeleteAllClientDataAsync("customer@example.com");
+            var result = response.Resource;
+            Console.WriteLine(result.Message);
+        }
+    }
+}
