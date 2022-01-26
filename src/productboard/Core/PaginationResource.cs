f4fb94485109591c6abedf69dfd45db44b66f96f
Maxwell Weru <mburumaxwell@gmail.com>
2022-01-26 13:11:42 +0300

    Added missing JsonPropertyName in PaginationResource and ResourceWithData

--- src/productboard/Core/PaginationResource.cs
+++ src/productboard/Core/PaginationResource.cs
@@ -1,9 +1,12 @@
-﻿namespace productboard.Core;
+﻿using System.Text.Json.Serialization;
+
+namespace productboard.Core;
 
 /// <summary>Represents a paginated response.</summary>
 /// <typeparam name="T">The type fo data contained.</typeparam>
 public class PaginationResource<T> : ResourceWithData<T> where T : class
 {
     /// <summary>Link to the next page.</summary>
+    [JsonPropertyName("links")]
     public PaginationLinks? Next { get; set; }
 }
--- src/productboard/Core/ResourceWithData.cs
+++ src/productboard/Core/ResourceWithData.cs
@@ -1,9 +1,12 @@
-﻿namespace productboard.Core;
+﻿using System.Text.Json.Serialization;
+
+namespace productboard.Core;
 
 /// <summary>Represents a response with <c>data</c> node.</summary>
 /// <typeparam name="T">The type contained in the <c>data</c> node.</typeparam>
 public class ResourceWithData<T> where T : class
 {
     /// <inheritdoc/>
+    [JsonPropertyName("data")]
     public T? Data { get; set; }
 }
