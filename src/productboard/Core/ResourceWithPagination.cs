d05df1923469046180cde77ebffa62075ad267ac
Maxwell Weru <mburumaxwell@gmail.com>
2022-01-26 12:59:28 +0300

    Added ResourceWithPagination

---
+++ src/productboard/Core/ResourceWithPagination.cs
@@ -0,0 +1,9 @@
+﻿namespace productboard.Core;
+
+/// <summary>Represents a paginated response.</summary>
+/// <typeparam name="T">The type fo data contained.</typeparam>
+public class ResourceWithPagination<T> : ResourceWithData<T> where T : class
+{
+    /// <summary>Link to the next page.</summary>
+    public PaginationLinks? Next { get; set; }
+}
