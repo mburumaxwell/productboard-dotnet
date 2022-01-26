68ec56771f8773bbe6952d7e91c6d036b2b2e94f
Maxwell Weru <mburumaxwell@gmail.com>
2022-01-26 13:13:13 +0300

    NoteCreationResult shoudl inherit from ResourceWithData

--- src/productboard/Models/NoteCreationResult.cs
+++ src/productboard/Models/NoteCreationResult.cs
@@ -1,17 +1,14 @@
-﻿using System.Text.Json.Serialization;
+﻿using productboard.Core;
+using System.Text.Json.Serialization;
 
 namespace productboard.Models;
 
 /// <summary>
 /// The result returned after creating a note
 /// </summary>
-public class NoteCreationResult : IHasResourceLinks
+public class NoteCreationResult : ResourceWithData<NoteData>, IHasResourceLinks
 {
     /// <summary>Links for accessing the created note.</summary>
     [JsonPropertyName("links")]
     public ResourceLinks? Links { get; set; }
-
-    /// <summary>Data about the created note</summary>
-    [JsonPropertyName("data")]
-    public NoteData? Data { get; set; }
 }
