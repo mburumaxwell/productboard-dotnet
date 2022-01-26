a9d4e8504b36b386c5d01ea03818b7564fee7876
Maxwell Weru <mburumaxwell@gmail.com>
2022-01-26 14:03:33 +0300

    Rename PaginationResponse -> PaginatedResponse

--- src/productboard/Core/PaginatedResponse.cs
+++ src/productboard/Core/PaginatedResponse.cs
@@ -4,7 +4,7 @@ namespace productboard.Core;
 
 /// <summary>Represents a paginated response.</summary>
 /// <typeparam name="T">The type fo data contained.</typeparam>
-public class PaginationResponse<T> : DataResource<T> where T : class
+public class PaginatedResponse<T> : DataResource<T> where T : class
 {
     /// <summary>Link to the next page.</summary>
     [JsonPropertyName("links")]
--- src/productboard/ProductboardClient.cs
+++ src/productboard/ProductboardClient.cs
@@ -73,11 +73,11 @@ public class ProductboardClient
     /// <param name="options"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    public async Task<ProductboardResponse<PaginationResponse<Feature>>> GetFeaturesAsync(FeaturesListOptions? options = null,
+    public async Task<ProductboardResponse<PaginatedResponse<Feature>>> GetFeaturesAsync(FeaturesListOptions? options = null,
                                                                                           CancellationToken cancellationToken = default)
     {
         var url = MakePathWithQuery("/features", options);
-        return await GetAsync<PaginationResponse<Feature>>(url, cancellationToken: cancellationToken);
+        return await GetAsync<PaginatedResponse<Feature>>(url, cancellationToken: cancellationToken);
     }
 
     /// <summary>Create a feature.</summary>
@@ -130,11 +130,11 @@ public class ProductboardClient
     /// <param name="options"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    public async Task<ProductboardResponse<PaginationResponse<Component>>> GetComponentsAsync(BasicListOptions? options = null,
+    public async Task<ProductboardResponse<PaginatedResponse<Component>>> GetComponentsAsync(BasicListOptions? options = null,
                                                                                               CancellationToken cancellationToken = default)
     {
         var url = MakePathWithQuery("/components", options);
-        return await GetAsync<PaginationResponse<Component>>(url, cancellationToken: cancellationToken);
+        return await GetAsync<PaginatedResponse<Component>>(url, cancellationToken: cancellationToken);
     }
 
     /// <summary>Get a component.</summary>
@@ -157,11 +157,11 @@ public class ProductboardClient
     /// <param name="options"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    public async Task<ProductboardResponse<PaginationResponse<Product>>> GetProductsAsync(BasicListOptions? options = null,
+    public async Task<ProductboardResponse<PaginatedResponse<Product>>> GetProductsAsync(BasicListOptions? options = null,
                                                                                           CancellationToken cancellationToken = default)
     {
         var url = MakePathWithQuery("/products", options);
-        return await GetAsync<PaginationResponse<Product>>(url, cancellationToken: cancellationToken);
+        return await GetAsync<PaginatedResponse<Product>>(url, cancellationToken: cancellationToken);
     }
 
     /// <summary>Get a product.</summary>
@@ -184,11 +184,11 @@ public class ProductboardClient
     /// <param name="options"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    public async Task<ProductboardResponse<PaginationResponse<FeatureStatus>>> GetFeatureStatusesAsync(BasicListOptions? options = null,
+    public async Task<ProductboardResponse<PaginatedResponse<FeatureStatus>>> GetFeatureStatusesAsync(BasicListOptions? options = null,
                                                                                                        CancellationToken cancellationToken = default)
     {
         var url = MakePathWithQuery("/feature-statuses", options);
-        return await GetAsync<PaginationResponse<FeatureStatus>>(url, cancellationToken: cancellationToken);
+        return await GetAsync<PaginatedResponse<FeatureStatus>>(url, cancellationToken: cancellationToken);
     }
 
     #endregion
